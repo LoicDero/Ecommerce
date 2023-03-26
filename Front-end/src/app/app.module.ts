ca0259e886d0c7fcace82673bfaecec34c8eb373
LoicDeroWastaken <loic.dero@gmail.com>
2023-03-26 18:20:12 +0200

    fini les paniers

--- Front-end/src/app/app.module.ts
+++ Front-end/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { SearchComponent } from './components/partials/search/search.component';
 import { TagsComponent } from './components/partials/tags/tags.component';
 import { ShoesPageComponent } from './components/pages/shoes-page/shoes-page.component';
 import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
+import { TitleComponent } from './components/partials/title/title.component';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { CartPageComponent } from './components/pages/cart-page/cart-page.compon
     SearchComponent,
     TagsComponent,
     ShoesPageComponent,
-    CartPageComponent
+    CartPageComponent,
+    TitleComponent
   ],
   imports: [
     BrowserModule,
