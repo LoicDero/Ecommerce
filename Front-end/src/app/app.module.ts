77918ca4183940ff8194aea7ac72f77261cd76cb
DelimeThibault <t.delime@students.ephec.be>
2023-04-06 15:37:38 +0200

    Pages non trouvées

--- Front-end/src/app/app.module.ts
+++ Front-end/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { TagsComponent } from './components/partials/tags/tags.component';
 import { ShoesPageComponent } from './components/pages/shoes-page/shoes-page.component';
 import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
 import { TitleComponent } from './components/partials/title/title.component';
+import { NotFoundComponent } from './components/partials/not-found/not-found.component';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { TitleComponent } from './components/partials/title/title.component';
     TagsComponent,
     ShoesPageComponent,
     CartPageComponent,
-    TitleComponent
+    TitleComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
