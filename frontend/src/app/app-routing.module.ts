2979cc0447e0f6c5b97d81d68ee040235ba1de99
LoicDeroWastaken <l.dero@students.ephec.be>
2023-04-08 01:20:49 +0200

    frontend directory renamed

---
+++ frontend/src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CartPageComponent } from './components/pages/cart-page/cart-page.component';
+import { HomeComponent } from './components/pages/home/home.component';
+import { ShoesPageComponent } from './components/pages/shoes-page/shoes-page.component';
+import { LoginPageComponent } from './components/pages/login-page/login-page.component';
+
+const routes: Routes = [
+  {path:'', component:HomeComponent},
+  {path:'search/:searchTerm', component:HomeComponent},
+  {path:'tag/:tag', component:HomeComponent},
+  {path:'shoes/:id', component:ShoesPageComponent},
+  {path:'cart-page', component:CartPageComponent},
+  {path:'login', component:LoginPageComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ frontend/src/app/shared/constants/urls.ts
@@ -0,0 +1,7 @@
+const BASE_URL = 'http://localhost:5000';
+
+export const SHOES_URL = BASE_URL + '/api/shoes';
+export const SHOES_TAGS_URL = SHOES_URL + '/tags';
+export const SHOES_BY_SEARCH_URL = SHOES_URL + '/search/';
+export const SHOES_BY_TAG_URL = SHOES_URL + '/tag/';
+export const SHOES_BY_ID_URL = SHOES_URL + '/';
