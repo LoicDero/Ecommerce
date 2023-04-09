875ec0742fad50e8b03b2fdce5e1b08efec3b4ec
LoicDeroWastaken <loic.dero@gmail.com>
2023-04-09 23:21:04 +0200

    login to backend

--- frontend/src/app/shared/constants/urls.ts
+++ frontend/src/app/shared/constants/urls.ts
@@ -5,3 +5,5 @@ export const SHOES_TAGS_URL = SHOES_URL + '/tags';
 export const SHOES_BY_SEARCH_URL = SHOES_URL + '/search/';
 export const SHOES_BY_TAG_URL = SHOES_URL + '/tag/';
 export const SHOES_BY_ID_URL = SHOES_URL + '/';
+
+export const USER_LOGIN_URL = BASE_URL + '/api/users/login';
