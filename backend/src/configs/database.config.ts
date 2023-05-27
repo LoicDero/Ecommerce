547396fd4589cad3ba15148f5705735386abdbec
LoicDeroWastaken <loic.dero@gmail.com>
2023-05-27 09:44:44 +0200

    try to connect log

--- backend/src/configs/database.config.ts
+++ backend/src/configs/database.config.ts
@@ -1,11 +1,12 @@
 import {connect, ConnectOptions} from 'mongoose';
 
 export const dbConnect = () => {
+    console.log("Connecting to the DB")
     connect(process.env.MONGO_URI!, {
         useNewUrlParser: true,
         useUnifiedTopology: true
     } as ConnectOptions).then(
         () => console.log("Connect successfully to the DB"),
-        (error) => console.log(error)
+        (error) => console.log("Failed to connect to the DB")
     )
 }
