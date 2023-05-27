fb065dc623d7b87e9a7dd7de8da6935510049906
LoicDeroWastaken <loic.dero@gmail.com>
2023-05-27 09:16:01 +0200

    test push

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -12,28 +12,6 @@ dbConnect();
 
 import asyncHandler = require('express-async-handler');
 
-const swaggerAutogen = require('swagger-autogen')();
-
-const doc = {
-  info: {
-    title: 'Ecommerce',
-    description: 'API du site d\'ecommerce WeThePast',
-  },
-  host: 'localhost:5000',
-  schemes: ['http'],
-};
-
-const outputFile = '../swagger-output.json';
-const jsonFile = require(outputFile)
-//const endpointsFiles = ['./router/shoes.router.ts', './router/order.router.ts', './router/user.router.ts']
-const endpointsFiles = ['./server.ts']
-
-/* NOTE: if you use the express Router, you must pass in the 
-   'endpointsFiles' only the root file where the route starts,
-   such as index.js, app.js, routes.js, ... */
-
-
-const swaggerUi = require('swagger-ui-express');
 
 const app = express();
 app.use(express.json())
@@ -45,12 +23,6 @@ app.use(cors({
 app.use("/api/shoes", shoesRouter);
 app.use("/api/users", userRouter);
 app.use("/api/orders", orderRouter);
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(jsonFile));
-
-swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
-  require('./server.ts'); // Your project's root file
-});
 
 app.use(express.static('public'));
 app.get('*', (req, res) => {
