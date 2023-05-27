26c5f4b1bc1886bf39255c5def7f08bac49885ce
LoicDeroWastaken <loic.dero@gmail.com>
2023-05-27 09:36:02 +0200

    test requête

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -17,7 +17,7 @@ const app = express();
 app.use(express.json())
 app.use(cors({
   credentials: true,
-  origin: ["http://localhost:4200"]
+  origin: ["http://localhost:4200", "https://localhost:5000"]
 }));
 
 app.use("/api/shoes", shoesRouter);
